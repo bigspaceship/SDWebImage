f1e623e08e6f25a5f5bf959e34d5c8de18e76a1b
Benjamin Bojko <b.bojko@bigspaceship.com>
2012-11-14 13:27:58 -0500

    Fixed 'unsupported parameter combination' error in SDWebImageDecoder

--- SDWebImage/SDWebImageDecoder.m
+++ SDWebImage/SDWebImageDecoder.m
@@ -96,34 +96,41 @@ static SDWebImageDecoder *sharedInstance;
 
 + (UIImage *)decodedImageWithImage:(UIImage *)image
 {
+	// b.bojko@bigspaceship.com:
+	// fixed "unsupported parameter combination" errors
+	// by replacing this code with example from (essentially just
+	// always use 4 bytes for argb instead of rgb occasionally)
+	// http://ioscodesnippet.tumblr.com/post/10924101444/force-decompressing-uiimage-in-background-to-achieve
+	
     CGImageRef imageRef = image.CGImage;
+    // System only supports RGB, set explicitly and prevent context error
+    // if the downloaded image is not the supported format
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGImageAlphaInfo alphaInfo = CGImageGetAlphaInfo(imageRef);
-
-    BOOL imageHasAlphaInfo = (alphaInfo != kCGImageAlphaNone);
-
-    int bytesPerPixel = imageHasAlphaInfo ? 4 : 3;
-    CGBitmapInfo bitmapInfo = imageHasAlphaInfo ? kCGImageAlphaPremultipliedLast : kCGImageAlphaNone;
-
+	
     CGContextRef context = CGBitmapContextCreate(NULL,
                                                  CGImageGetWidth(imageRef),
                                                  CGImageGetHeight(imageRef),
                                                  8,
-                                                 // Just always return width * bytesPerPixel will be enough
-                                                 CGImageGetWidth(imageRef) * bytesPerPixel,
-                                                 // System only supports RGB, set explicitly
+                                                 // width * 4 will be enough because are in ARGB format, don't read from the image
+                                                 CGImageGetWidth(imageRef) * 4,
                                                  colorSpace,
-                                                 bitmapInfo);
+                                                 // kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Little
+                                                 // makes system don't need to do extra conversion when displayed.
+                                                 kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Little);
     CGColorSpaceRelease(colorSpace);
-    if (!context) return nil;
-
-    CGRect rect = (CGRect){CGPointZero,{CGImageGetWidth(imageRef), CGImageGetHeight(imageRef)}};
+	
+    if ( ! context) {
+        return nil;
+    }
+    
+    CGRect rect = (CGRect){CGPointZero, {CGImageGetWidth(imageRef), CGImageGetHeight(imageRef)}};
     CGContextDrawImage(context, rect, imageRef);
     CGImageRef decompressedImageRef = CGBitmapContextCreateImage(context);
     CGContextRelease(context);
-
-    UIImage *decompressedImage = [[UIImage alloc] initWithCGImage:decompressedImageRef scale:image.scale orientation:image.imageOrientation];
+    
+    UIImage *decompressedImage = [[UIImage alloc] initWithCGImage:decompressedImageRef];
     CGImageRelease(decompressedImageRef);
+//    return [decompressedImage autorelease];
     return SDWIReturnAutoreleased(decompressedImage);
 }
 
